356857efaf80d32c450776b8d8d3114bac0a43b2
MasumRezaMunna <masumrezamunna2005@gmail.com>
2026-06-25 18:41:04 +0600

    updated demo user and admin email

--- lib/demoAccounts.ts
+++ lib/demoAccounts.ts
@@ -1,5 +1,5 @@
-export const DEMO_USER_EMAIL = "demo.user@wanderlusttrails.example";
+export const DEMO_USER_EMAIL = "demo.user@gmail.com";
 export const DEMO_USER_PASSWORD = "DemoUser123!";
 
-export const DEMO_ADMIN_EMAIL = "demo.admin@wanderlusttrails.example";
+export const DEMO_ADMIN_EMAIL = "demo.admin@gmail.com";
 export const DEMO_ADMIN_PASSWORD = "DemoAdmin123!";
